53fa185aa93b332abb0508ffc59def4816097ef6
junyi <q5896799@gmail.com>
2019-04-14 13:51:09 +0800

    'debug'

---
+++ 第五章 系統穩定度分析/HW05.m
@@ -0,0 +1,56 @@
+clc;clear
+C=4
+% % 1
+% % (a)
+% G1_a=tf([1 8],[1 -1 4 -4 3*C -2])
+% T_YR_1a=feedback(G1_a,1)
+% step(T_YR_1a)
+% Ans1_a=pole(T_YR_1a)
+% 
+% % (b)
+% G1_b=tf([1 4 -3],[1 4 8 20*C 15])
+% T_YR_1b=feedback(G1_b,1)
+% step(T_YR_1b)
+% Ans1_b=pole(T_YR_1b)
+% 
+% % (c)
+% G1_c=tf([1 2 7 21],[1 -1 3 -3 2*C -2])
+% T_YR_1c=feedback(G1_c,1)
+% step(T_YR_1c)
+% Ans1_c=pole(T_YR_1c)
+% 
+% % 2
+% % (a)
+% G2_a=tf([4],[1 -2 2 -4 -1 2*C -2 0])
+% T_YR_2a=feedback(G2_a,1)
+% step(T_YR_2a)
+% Ans1_a=pole(T_YR_2a)
+% 
+% % (b)
+% G2_b=tf([8],[1 -2 -1 2 4 -8*C -4 0])
+% T_YR_2b=feedback(G2_b,1)
+% step(T_YR_2b)
+% Ans1_b=pole(T_YR_2b)
+% 
+% % (c)
+% G2_c=tf([507],[1 3 10 30*C 169])
+% T_YR_2c=feedback(G2_c,1)
+% step(T_YR_2c)
+% Ans1_c=pole(T_YR_2c)
+
+% 3
+% (a)
+
+
+% (b)
+
+
+% (c)
+
+
+
+% % 4
+% A4=[2 0 -1;0 4 6;-6 -5 C]
+% Ans4=eig(A4)
+
+% 5
