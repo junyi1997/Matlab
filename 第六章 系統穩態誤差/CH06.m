1b45b4a6881e42749b4545164aa7c05eb9157e3f
junyi <q5896799@gmail.com>
2019-05-19 21:38:09 +0800

    'first'

--- 第六章 系統穩態誤差/CH06.m
+++ 第六章 系統穩態誤差/CH06.m
@@ -2,46 +2,47 @@ clc;clear
 C=4
 % 1
 % (a)
-G_1a=tf(conv(20,poly(-3 -C -8)),conv([1 0 0],poly(-2 -15)));
-T_1a=feedback(G_1a,1);
-poles=pole(T_1a)
-Kp_1a=dcgain(G_1a)
-ess_step_1a=1/(1+Kp_1a)
-S_1_1a=tf([1 0],1);
-SG_1a=minreal(S_1_1a*G_1a);
-Kv_1a=dcgain(SG_1a)
-ess_Ramp_1a=1/Kv_1a
-S_2_1a=tf([1 0 0],1);
-S2G_1a=minreal(S_2_1a*G_1a);
-Ka_1a=dcgain(S2G_1a)
-ess_1a=1/Ka_1a
+% G_1a=tf(conv(20,poly([-3 -C -8])),conv([1 0 0],poly([-2 -15])));
+% T_1a=feedback(G_1a,1);
+% step(T_1a)
+% poles=pole(T_1a)
+% S_2_1a=tf([1 0 0],1);
+% S2G_1a=minreal(S_2_1a*G_1a);
+% Ka_1a=dcgain(S2G_1a)
+% ess_1a=1/Ka_1a
 
 % (b)
-G_1b=tf(500*C,conv([1 20],[1 4 10]));
-T_1b=feedback(G_1b,1);
-poles=pole(T_1b)
-Kp_1b=dcgain(G_1b)
-ess_step_1b=1/(1+Kp_1b)
-S_1_1b=tf([1 0],1);
-SG_1b=minreal(S_1_1b*G_1b);
-Kv_1b=dcgain(SG_1b)
-ess_Ramp_1b=1/Kv_1b
-S_2_1b=tf([1 0 0],1);
-S2G_1b=minreal(S_2_1b*G_1b);
-Ka_1b=dcgain(S2G_1b)
-ess_1b=1/Ka_1b
+% G_1b=tf(500*C,conv([1 20],[1 4 10]));
+% T_1b=feedback(G_1b,1);
+% poles=pole(T_1b)
+% Kp_1b=dcgain(G_1b)
+% ess_step_1b=40/(1+Kp_1b)
+% S_1_1b=tf([1 0],1);
+% SG_1b=minreal(S_1_1b*G_1b);
+% Kv_1b=dcgain(SG_1b)
+% ess_Ramp_1b=70/Kv_1b
+% S_2_1b=tf([1 0 0],1);
+% S2G_1b=minreal(S_2_1b*G_1b);
+% Ka_1b=dcgain(S2G_1b)
+% ess_1b=80*2/Ka_1b
 
-% (c)
-G_1c=tf(conv(1000,poly(-12 -C -8)),poly(-61 -73 -87));
-T_1c=feedback(G_1c,1);
-poles=pole(T_1c)
-Kp_1c=dcgain(G_1c)
-ess_step_1c=1/(1+Kp_1c)
-S_1_1c=tf([1 0],1);
-SG_1c=minreal(S_1_1c*G_1c);
-Kv_1c=dcgain(SG_1c)
-ess_Ramp_1c=1/Kv_1c
-S_2_1c=tf([1 0 0],1);
-S2G_1c=minreal(S_2_1c*G_1c);
-Ka_1c=dcgain(S2G_1c)
-ess_1c=1/Ka_1c
+% % (c)
+% G_1c=tf(conv(1000,poly([-12 -C -32])),poly([-61 -73 -87]));
+% T_1c=feedback(G_1c,1);
+% poles=pole(T_1c)
+% Kp_1c=dcgain(G_1c)
+% ess_step_1c=15/(1+Kp_1c)
+
+% 2
+G1_2=tf(1,[1 0])
+G2_2=tf(5,[1 1])
+H1_2=C
+H2_2=tf([1 3],1)
+G3_2=feedback(G2_2,H1_2)
+G_2=feedback(G3_2*G1_2,H2_2)
+T_2=tf(G_2,1)
+step(T_2)
+'step'
+poles=pole(T_2)
+Kp_2=dcgain(G_2)
+ess_step_2=15/(1+Kp_2)
